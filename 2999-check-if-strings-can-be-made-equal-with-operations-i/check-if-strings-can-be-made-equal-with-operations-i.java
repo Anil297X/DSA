30aa773940c10aecc149244a3ba8a1e698d6b4cc
ANIL KUMAR <83276957+Anil297X@users.noreply.github.com>
2026-03-30 23:30:12 +0530

    Time: 2 ms (29.69%) | Memory: 44.3 MB (57.81%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        if(s1.equals(s2)) return true;
-        for(int i=0;i<4;i++){
-            if(s1.charAt(i)!=s2.charAt(i)){
-              
-                if(s1.charAt(i)!=s2.charAt((i+2)%4)) return false;
-            }
-        }
-        return true;
+        char[] even1 = new char[]{s1.charAt(0), s1.charAt(2)};
+        char[] even2 = new char[]{s2.charAt(0), s2.charAt(2)};
+        
+        char[] odd1 = new char[]{s1.charAt(1), s1.charAt(3)};
+        char[] odd2 = new char[]{s2.charAt(1), s2.charAt(3)};
+        
+        Arrays.sort(even1);
+        Arrays.sort(even2);
+        Arrays.sort(odd1);
+        Arrays.sort(odd2);
+        
+        return Arrays.equals(even1, even2) &&
+               Arrays.equals(odd1, odd2);        
     }
 }
